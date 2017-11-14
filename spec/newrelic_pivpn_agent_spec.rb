dafe05f508603fb38f35cd330644c6cb88398309
zolwilhe <zolwilhe@ie.ibm.com>
2017-11-14 13:20:46 +0000

    specs

---
+++ spec/newrelic_pivpn_agent_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe NewrelicPivpnAgent do
+  it "has a version number" do
+    expect(NewrelicPivpnAgent::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
